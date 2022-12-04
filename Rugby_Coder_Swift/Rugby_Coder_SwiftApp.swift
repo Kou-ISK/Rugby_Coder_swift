6393161305303d74a56840190d2f66cfef033060
k-isaka <isk.rugby@gmail.com>
2022-12-04 20:56:56 +0900

    Initial Commit

---
+++ Rugby_Coder_Swift/Rugby_Coder_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rugby_Coder_SwiftApp.swift
+//  Rugby_Coder_Swift
+//
+//  Created by 井坂航 on 2022/12/04.
+//
+
+import SwiftUI
+
+@main
+struct Rugby_Coder_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
